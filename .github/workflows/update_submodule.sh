b96c55bbf193a3801f3d9b5c74c059d7473cdf9c
Nishanth Gobi <nish.professional@gmail.com>
2024-05-10 10:10:42 +0530

    refactor update logic to separate script file

---
+++ .github/workflows/update_submodule.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git config --global user.email "nish.professional@gmail.com"
+git config --global user.name "Nishanth-Gobi"
+
+# git submodule add git@github.com:Nishanth-Gobi/Obsidian-Vault.git content
+git submodule update --remote --merge
+git add --all
+
+if [[ $(git status --porcelain) ]]; then
+    git commit -m "Update content"
+    git push
+else
+    echo "No changes to commit and push."
+fi
